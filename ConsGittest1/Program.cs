192874055cff708b77a20f66dd9781151b606dfa
SFR <soren@francis.dk>
2016-08-10 13:49:21 +0200

    første forsøg

--- ConsGittest1/Program.cs
+++ ConsGittest1/Program.cs
@@ -10,6 +10,10 @@ namespace ConsGittest1
     {
         static void Main(string[] args)
         {
+            gClass tut = new gClass() { id=3, navn="kurt"} ;
+            Console.WriteLine("Hello 1.0");
+            Console.ReadLine();
+
         }
     }
 }
---
+++ ConsGittest1/gClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsGittest1
+{
+    class gClass
+    {
+        public int id { get; set; }
+        public string navn { get; set; }
+    }
+}
